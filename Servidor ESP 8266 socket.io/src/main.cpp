c27b736908756b0679a4bf3c4c2afeaba5e75b87
DrStone4384 <135196724+DrStone438@users.noreply.github.com>
2025-01-14 20:34:19 -0300

    no se

--- Servidor ESP 8266 socket.io/src/main.cpp
+++ Servidor ESP 8266 socket.io/src/main.cpp
@@ -1,65 +1,80 @@
 #include <ESP8266WiFi.h>
 #include <WebSocketsClient.h>
-// Wifi Casa
-const char* ssid = "Casa Bello";
-const char* password = "N66k8nbe";
+#include <arduino.h>
 
-//Wifi Trabajo
-//const char* ssid = "Grupo EPM";
-//const char* password = "Soporte632";
+// Wifi Casa
+const char *ssid = "Casa Bello";
+const char *password = "N66k8nbe";
+
+// Wifi Trabajo
+// const char* ssid = "Grupo EPM";
+// const char* password = "Soporte632";
 
 // IP de Casa
-const char* serverIp = "192.168.100.13"; // Cambia por la IP del servidor
+const char *serverIp = "192.168.100.13"; // Cambia por la IP del servidor
 // IP de Trabajo
-//const char* serverIp = "201.236.162.98";
-
+// const char* serverIp = "201.236.162.98";
 
 const int serverPort = 3000;
 WebSocketsClient webSocket;
 
-void webSocketEvent(WStype_t type, uint8_t* payload, size_t length) {
-    switch (type) {
-        case WStype_DISCONNECTED:
-            Serial.println("Desconectado del servidor");
-            break;
-        case WStype_CONNECTED:
-            Serial.println("Conectado al servidor WebSocket");
-            webSocket.sendTXT("ESP conectado");
-            break;
-        case WStype_TEXT:
-            Serial.println("Mensaje recibido desde WebSocket");
-            Serial.printf("Payload recibido: %s\n", (char*)payload);
-            String command = String((char*)payload);
-            Serial.println("Comando recibido: " + command);
+void webSocketEvent(WStype_t type, uint8_t *payload, size_t length)
+{
+    switch (type)
+    {
+    case WStype_DISCONNECTED:
+        Serial.println("Desconectado del servidor");
+        break;
+    case WStype_CONNECTED:
+        Serial.println("Conectado al servidor WebSocket");
+        webSocket.sendTXT("ESP conectado");
+        break;
+    case WStype_TEXT:
+        Serial.println("Mensaje recibido desde WebSocket");
+        Serial.printf("Payload recibido: %s\n", (char *)payload);
+        String command = String((char *)payload);
+        Serial.println("Comando recibido: " + command);
 
-
-            // Ejecutar el comando
-            if ((char*)payload == "adelante") {
-                // Lógica para mover adelante
-                digitalWrite(D1, HIGH);
-                Serial.println("adelante");
-            } else if (command == "atras") {
-                // Lógica para mover atrás
-            } else if (command == "izquierda") {
-                // Lógica para girar izquierda
-            } else if (command == "derecha") {
-                // Lógica para girar derecha
-            } else if (command == "stop") {
-                // Lógica para detener
-                digitalWrite(D1, LOW);
-            }
-            Serial.println("Mensaje recibido desde WebSocket");
-            Serial.printf("Payload recibido 2: %s\n", (char*)payload);
-            break;
+        // Ejecutar el comando
+        if (command.equals("adelante"))
+        {
+            // Lógica para mover adelante
+            digitalWrite(D1, HIGH);
+            Serial.println("adelante");
+        }
+        else if (command.equals("atras"))
+        {
+            // Lógica para mover atrás
+            Serial.println("atras");
+        }
+        else if (command.equals("izquierda"))
+        {
+            // Lógica para girar izquierda
+            Serial.println("izquierda");
+        }
+        else if (command.equals("derecha"))
+        {
+            // Lógica para girar derecha
+            Serial.println("derecha");
+        }
+        else if (command.equals("stop"))
+        {
+            // Lógica para detener
+            digitalWrite(D1, LOW);
+            Serial.println("stop");
+        }
+        break;
     }
 }
 
-void setup() {
+void setup()
+{
     Serial.begin(115200);
 
     // Conexión WiFi
     WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) {
+    while (WiFi.status() != WL_CONNECTED)
+    {
         delay(1000);
         Serial.println("Conectando a WiFi...");
     }
@@ -72,6 +87,7 @@ void setup() {
     pinMode(D1, OUTPUT);
 }
 
-void loop() {
-    webSocket.loop();  // Mantener conexión activa
+void loop()
+{
+    webSocket.loop(); // Mantener conexión activa
 }
