d04b8d5b9a894d42434d560f0310aa7d6cf45002
DrStone4384 <135196724+DrStone438@users.noreply.github.com>
2025-01-14 18:43:44 -0300

    Mejore la pagina para ponerlo en una esquina

--- Servidor ESP 8266 socket.io/src/main.cpp
+++ Servidor ESP 8266 socket.io/src/main.cpp
@@ -1,27 +1,20 @@
 #include <ESP8266WiFi.h>
 #include <WebSocketsClient.h>
-/*Wifi Casa
+// Wifi Casa
 const char* ssid = "Casa Bello";
 const char* password = "N66k8nbe";
-*/
+
 //Wifi Trabajo
 //const char* ssid = "Grupo EPM";
 //const char* password = "Soporte632";
-//Wifi Telefono
-const char* ssid = "S21 de Joaquin";
-const char* password = "pdxm5512";
 
 // IP de Casa
-//const char* serverIp = "192.168.100.13"; // Cambia por la IP del servidor
+const char* serverIp = "192.168.100.13"; // Cambia por la IP del servidor
 // IP de Trabajo
 //const char* serverIp = "201.236.162.98";
 
-// Ip del telefono
-const char* serverIp = "186.11.80.196";
+
 const int serverPort = 3000;
-
-
-
 WebSocketsClient webSocket;
 
 void webSocketEvent(WStype_t type, uint8_t* payload, size_t length) {
