1811cbdb626d8a9392062f26e337a4fe296628f4
DrStone4384 <135196724+DrStone438@users.noreply.github.com>
2025-01-22 23:40:07 -0300

    Fusione el original con el multiUser

--- ESP 8266/src/main.cpp
+++ ESP 8266/src/main.cpp
@@ -31,54 +31,57 @@ void webSocketEvent(WStype_t type, uint8_t *payload, size_t length)
         Serial.println("Conectado al servidor WebSocket");
         delay(500);// deja un ligero retraso para que el servidor pueda responder
         webSocket.sendTXT("ESP conectado");
+        webSocket.sendTXT("register:robot:robot1");
         break;
     case WStype_TEXT:
         Serial.println("Mensaje recibido desde WebSocket");
-        Serial.printf("Payload recibido: %s\n", (char *)payload);
         String command = String((char *)payload);
         Serial.println("Comando recibido: " + command);
 
-        // Ejecutar el comando
-        if (command.equals("adelante"))
+        if (command.startsWith("command:")) 
         {
-            // Lógica para mover adelante
-            Serial.println("Comando: adelante detectado");
-            digitalWrite(D1, HIGH);
-            Serial.println("adelante");
-            webSocket.sendTXT("Comando ejecutado: adelante");// manda un mensaje al servidor de que se ejecuto el comando
+            String command = command.substring(8);
+            if (command.equals("adelante"))
+            {
+                // Lógica para mover adelante
+                Serial.println("Comando: adelante detectado");
+                digitalWrite(D1, HIGH);
+                Serial.println("adelante");
+                webSocket.sendTXT("Comando ejecutado: adelante");// manda un mensaje al servidor de que se ejecuto el comando
+            }
+            else if (command.equals("atras"))
+            {
+                // Lógica para mover atrás
+                Serial.println("atras");
+                digitalWrite(D2,HIGH);
+            }
+            else if (command.equals("izquierda"))
+            {
+                // Lógica para girar izquierda
+                Serial.println("izquierda");
+                digitalWrite(D3,HIGH);
+            }
+            else if (command.equals("derecha"))
+            {
+                // Lógica para girar derecha
+                Serial.println("derecha");
+                digitalWrite(D4,HIGH);
+            }
+            else if (command.equals("stop"))
+            {
+                // Lógica para detener
+                digitalWrite(D1, LOW);
+                digitalWrite(D2, LOW);
+                digitalWrite(D3, LOW);
+                digitalWrite(D4, LOW);
+                Serial.println("stop");
+            }
+            else
+            {
+                Serial.println("Comando no reconocido: " + command);
+            }
+            break;
         }
-        else if (command.equals("atras"))
-        {
-            // Lógica para mover atrás
-            Serial.println("atras");
-            digitalWrite(D2,HIGH);
-        }
-        else if (command.equals("izquierda"))
-        {
-            // Lógica para girar izquierda
-            Serial.println("izquierda");
-            digitalWrite(D3,HIGH);
-        }
-        else if (command.equals("derecha"))
-        {
-            // Lógica para girar derecha
-            Serial.println("derecha");
-            digitalWrite(D4,HIGH);
-        }
-        else if (command.equals("stop"))
-        {
-            // Lógica para detener
-            digitalWrite(D1, LOW);
-            digitalWrite(D2, LOW);
-            digitalWrite(D3, LOW);
-            digitalWrite(D4, LOW);
-            Serial.println("stop");
-        }
-        else
-        {
-            Serial.println("Comando no reconocido: " + command);
-        }
-        break;
     }
 }
 
